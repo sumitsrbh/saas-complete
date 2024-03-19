b5bff5e7a88131a3e772baf8c72711de0aec3746
sourabh <sumitsrbh@gmail.com>
2024-03-19 15:15:41 +0530

    delted unwanted component

--- Client/saasproj/src/Components/PromoteYourSaaS.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function PromoteYourSaaS() {
-  return <div>PromoteYourSaaS</div>
-}
-
-export default PromoteYourSaaS
--- Client/saasproj/src/Components/WriteFrUs.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function WriteFrUs() {
-  return <div>WriteFrUs</div>
-}
-
-export default WriteFrUs
