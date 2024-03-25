94cde8cdb5ab7d605f74e6f1ea63c73bc2678e6d
sourabh <sumitsrbh@gmail.com>
2024-03-25 20:42:31 +0530

    added badge to cardbuilderV2, but image does not take equla width

--- Client/saasproj/src/Components/CardBuilder/CardBuilderV2.js
+++ Client/saasproj/src/Components/CardBuilder/CardBuilderV2.js
@@ -33,7 +33,7 @@ export function CardBuidlerV2({
           component="img"
           image={cardImgUrl}
           style={{
-            Width: '250px',
+            width: '100%',
             height: '220px',
             transition: 'transform 0.5s ease',
             transform: 'translateX(0)',
